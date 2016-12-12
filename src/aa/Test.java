fa93036eee54efce3dee71466e974bbff60ceff1
chaijinhu <chaijinhu@USER-20160823HT>
2016-12-12 14:37:25 +0800

    ok

--- src/aa/Test.java
+++ src/aa/Test.java
@@ -11,6 +11,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
@@ -18,8 +21,12 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Test {
+//	public static void main(String[] args) {
+//		String aa = "原价25.9元，抢券立省3元";
+//		System.out.println();
+//	}
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException {
-		String url="http://tbb.so/i0PPGO";
+		String url="http://tbb.so/i0PMHR";
 		WebClient webClient = new WebClient(BrowserVersion.FIREFOX_45);
 	    //设置webClient的相关参数
 	    webClient.getOptions().setJavaScriptEnabled(true);
@@ -34,6 +41,23 @@ public class Test {
 	    System.out.println("线程结束沉睡");
 	    String html = rootPage.asXml();
 	    System.out.println(html);
+	    Document doc = Jsoup.parse(html, "UTF-8");
+	    String picURL = doc.select("div.pic img").first().attr("src");
+	    String attr = doc.select("div[mx-view=app/common/share/index]").first().attr("mx-init");
+	    String[] date =attr.substring((attr.indexOf('"')+1),attr.lastIndexOf('"')).split("  ");
+	    String dealNumHtml = doc.select("span.dealNum").first().html();
+	    String dealNum = dealNumHtml.substring(0,dealNumHtml.indexOf("笔成交"));
+	    System.out.println("图片URL    "+picURL);
+	    System.out.println("productName  "+date[0]);
+	    //价钱
+	    System.out.println("价钱     "+date[1]);
+	    //优惠券
+	    System.out.println("优惠券"+date[1].substring((date[1].indexOf("省")+1),date[1].lastIndexOf("元")));
+	    //销量
+	    System.out.println("销量    "+dealNum);
+	    //抢券链接
+	    System.out.println("抢券链接    "+url);
+	    
 	}
 	/*public static void main3(String[] args) throws Exception {
 		BufferedReader reader =null;
