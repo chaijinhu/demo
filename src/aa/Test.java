279d5c5602541853f3a7878b97536f0a431262f0
chaijinhu <jinhu358@163.com>
2016-12-13 20:22:44 +0800

    Update Test.java

--- src/aa/Test.java
+++ src/aa/Test.java
@@ -23,6 +23,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.ScriptException;
 import com.gargoylesoftware.htmlunit.WebClient;
@@ -47,7 +48,7 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		 
 		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(20); 
-		File f = new File("C:\\Users\\Administrator\\Desktop\\a.txt");
+		File f = new File("C:\\Users\\Administrator\\Desktop\\b.txt");
 		ArrayList<String> urlDate = initQQDate(f);
 		String url =null;
 		if(urlDate.size()!=0){
@@ -68,7 +69,7 @@ public class Test {
 			
 			
 			JSONArray jsonArray = JSONArray.fromObject( Test.products ); 
-			BufferedWriter write = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("d:/products.txt")) ));
+			BufferedWriter write = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("d:/products.json")),"utf-8" ));
 			write.write("var products ="+jsonArray.toString());
 			write.flush();
 			write.close();
@@ -111,114 +112,67 @@ public class Test {
 	
 	
 
-	
-	public static void main6(String[] args) throws Exception {
-		String url="http://tbb.so/i0PMHR";
-		WebClient webClient = new WebClient(BrowserVersion.FIREFOX_45);
+	public static void main2(String[] args) {
+		WebClient	webClient = new WebClient(BrowserVersion.FIREFOX_45);
 	    //设置webClient的相关参数
 	    webClient.getOptions().setJavaScriptEnabled(true);
 	    webClient.getOptions().setCssEnabled(false);
 	    webClient.setAjaxController(new NicelyResynchronizingAjaxController());
 	    //webClient.getOptions().setTimeout(50000);
 	    webClient.getOptions().setThrowExceptionOnScriptError(false);
-	    webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-	    //模拟浏览器打开一个目标网址
-	    HtmlPage rootPage=null;
-	    try{
-	    rootPage = webClient.getPage(url);
-	    System.out.println("为了获取js执行的数据 线程开始沉睡等待");
-	    Thread.sleep(3000);//主要是这个线程的等待 因为js加载也是需要时间的
-	    }catch(ScriptException e){
-	    	System.out.println("---------------------/n-----------------------------");
-	    }catch (Exception e) {
-			throw e;
-		}
-	  
-	    System.out.println("线程结束沉睡");
-	    String html = rootPage.asXml();
-	    System.out.println(html);
-	    Document doc = Jsoup.parse(html, "UTF-8");
-	    String picURL = doc.select("div.pic img").first().attr("src");
-	    String attr = doc.select("div[mx-view=app/common/share/index]").first().attr("mx-init");
-	    String[] date =attr.substring((attr.indexOf('"')+1),attr.lastIndexOf('"')).split("  ");
-	    String dealNumHtml = doc.select("span.dealNum").first().html();
-	    String dealNum = dealNumHtml.substring(0,dealNumHtml.indexOf("笔成交"));
-	    System.out.println("图片URL    "+picURL);
-	    System.out.println("productName  "+date[0]);
-	    //价钱str
-	    System.out.println("价钱     "+date[1]);
-	    //原价
-	    String priceOld = date[1].substring((date[1].indexOf("价")+1),date[1].indexOf("元"));
-	    System.out.println(priceOld);
-	    //优惠券
-	    String priceNew = date[1].substring((date[1].indexOf("省")+1),date[1].lastIndexOf("元"));
-	    System.out.println("优惠券  "+priceNew);
-	    //销量
-	    System.out.println("销量    "+dealNum);
-	    //抢券链接
-	    System.out.println("抢券链接    "+url);
-	    //券后价格
-	    System.out.println("券后价格     "+(Double.valueOf(priceOld)-Double.valueOf(priceNew))+"");
-	}
-	/*public static void main3(String[] args) throws Exception {
-		BufferedReader reader =null;
-		BufferedWriter write = null;
-		 URL  url =  new URL("http://tbb.so/i0PPGO");
-		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); 
-		int responseCode = urlConnection.getResponseCode();
-		if(200==responseCode){
-			reader = new BufferedReader (new InputStreamReader( urlConnection.getInputStream(),"UTF-8"));
-			//write = new BufferedWriter((new OutputStreamWriter(new FileOutputStream(new File("D://test.txt")),"utf-8" )));
-			String line;
-			while((line = reader.readLine())!=null){
-				//write.write(line);
-				//write.newLine();
-				if(line.contains("mm_119813440_18296586_65138761")){
-					line = line.substring((line.indexOf('"')+1),line.lastIndexOf('"'));
-					break;
-				}else{
-					continue;
-				}
+		String url="http://tbb.so/m0M5RV";
+		try{
+			// TODO Auto-generated method stub
+			HtmlPage rootPage =null;
+			 //模拟浏览器打开一个目标网址
+		    System.out.println("为了获取js执行的数据 线程开始沉睡等待");
+		    try {
+		    	rootPage = webClient.getPage(url);
+				Thread.sleep(5000);
+			} catch (InterruptedException | FailingHttpStatusCodeException | IOException e) {
+				e.printStackTrace();
+			}//主要是这个线程的等待 因为js加载也是需要时间的
+		    System.out.println("线程结束沉睡");
+		    String html = rootPage.asXml();
+		    //System.out.println(html);
+		    Document doc = Jsoup.parse(html, "UTF-8");
+		    String picURL=null;
+		    String attr=null;
+		    String[] date=null;
+		    String dealNumHtml=null;
+		    try{
+			    picURL = doc.select("div.pic img").first().attr("src");
+			    attr = doc.select("div[mx-view=app/common/share/index]").first().attr("mx-init");
+			    date =attr.substring((attr.indexOf('"')+1),attr.lastIndexOf('"')).split("  ");
+			    dealNumHtml = doc.select("span.dealNum").first().html();
+		    }catch(Exception e){
+		    	return;
+		    }
+		    String dealNum = dealNumHtml.substring(0,dealNumHtml.indexOf("笔成交"));
+		    System.out.println("图片URL    "+picURL);
+		    System.out.println("productName  "+date[0]);
+		    //价钱str
+		    System.out.println("价钱     "+date[1]);
+		    //原价
+		    String priceOld = date[1].substring((date[1].indexOf("价")+1),date[1].indexOf("元"));
+		    System.out.println(priceOld);
+		    //优惠券
+		    String priceNew = date[1].substring((date[1].indexOf("省")+1),date[1].lastIndexOf("元"));
+		    System.out.println("优惠券  "+priceNew);
+		    if(priceNew.equals("null")){
+		    	System.out.println("priceNew.equals(null)");
+		    	return ;
+		    }
+		    //销量
+		    System.out.println("销量    "+dealNum);
+		    //抢券链接
+		    System.out.println("抢券链接    "+url);
+		    //券后价格
+		    String afterPrice = (Double.valueOf(priceOld)-Double.valueOf(priceNew))+"";
+		    System.out.println("券后价格     "+afterPrice);
+		    Test.products.add(new Product(""+(Test.products.size()+1), "https:"+picURL, date[0], priceOld, priceNew, afterPrice, dealNum, url));
+			}catch(Exception e){
+				e.printStackTrace();
 			}
-			System.out.println(line);
-			get(line);
-			
-		}else{
-			
-			 System.out.println("获取不到网页的源码，服务器响应代码为："+responseCode);
-		}
-		reader.close();
 	}
-	
-	
-	
-	
-	public static void  get(String urll) throws Exception{
-		BufferedReader reader =null;
-		BufferedWriter write = null;
-		 URL  url =  new URL(urll);
-		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); 
-		int responseCode = urlConnection.getResponseCode();
-		if(200==responseCode){
-			reader = new BufferedReader (new InputStreamReader( urlConnection.getInputStream(),"UTF-8"));
-			write = new BufferedWriter((new OutputStreamWriter(new FileOutputStream(new File("D://test.txt")),"utf-8" )));
-			String line;
-			while((line = reader.readLine())!=null){
-				write.write(line);
-				write.newLine();
-//				if(line.contains("mm_119813440_18296586_65138761")){
-//					line = line.substring((line.indexOf('"')+1),line.lastIndexOf('"'));
-//					break;
-//				}else{
-//					continue;
-//				}
-			}
-			write.flush();
-		}else{
-			
-			 System.out.println("获取不到网页的源码，服务器响应代码为："+responseCode);
-		}
-		reader.close();
-		write.close();
-	}*/
 }
